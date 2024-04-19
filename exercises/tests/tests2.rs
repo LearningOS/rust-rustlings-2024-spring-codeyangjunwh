ac070d6d2ebe35562fa3a49d8dc8a29264ce87d9
codeyangjunwh <codeyangjunwh@gmail.com>
2024-04-19 15:02:29 +0800

    update

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -6,12 +6,12 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let e1 = 45;
+        let e2 = 45;
+        assert_eq!(e1, e2);
     }
 }
